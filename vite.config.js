ce62ef05d869f71cfbf3b2c667a68669e3dd093c
yordanossole <yordanos.sole@gmail.com>
2026-05-23 10:27:04 +0300

    fix: allowed urls added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // important (allows external access)
+    port: 5173,
+    allowedHosts: [
+      'calamity-spearfish-shorthand.ngrok-free.dev',
+      '*'
+    ]
+  }
 })
